add20402748c51d23f6a1440c5815205fb9f7bbc
Mariano Richards <marianorichards@gmail.com>
2026-01-23 00:41:38 -0300

    delayed animatins: version 1 - 003

--- js/typewriter-logo.js
+++ js/typewriter-logo.js
@@ -40,9 +40,9 @@
     walk(temp);
 
     // --- Timing ---
-    const baseDelay = 75;        // velocidad por letra
-    const lineBreakDelay = 0;  // pausa extra en <br>
-    const afterLoadDelay = 2500; // delay deseado después de load
+    const baseDelay = 60;        // velocidad por letra
+    const lineBreakDelay = 200;  // pausa extra en <br>
+    const afterLoadDelay = 1200; // delay deseado después de load
 
     let i = 0;
 
